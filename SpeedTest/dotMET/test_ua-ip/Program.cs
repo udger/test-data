bc5f0963fc7f7087c820e653c849f1ab7774aa8b
udger.com <info@udger.com>
2016-09-10 13:50:18 +0200

    Add IP test

--- SpeedTest/dotMET/test_ua-ip/Program.cs
+++ SpeedTest/dotMET/test_ua-ip/Program.cs
@@ -18,24 +18,57 @@ namespace test_ua_ip
         
         static void Main(string[] args)
         {
-            string line;
-            long start_time, elapsed_time;
-            Udger.Parser.UserAgent a;
-            Udger.Parser.IPAddress i;
+            string                  line;
+            long                    start_time, elapsed_time;
+            Udger.Parser.UserAgent  a;
+            Udger.Parser.IPAddress  i;
+            WebClient               client;
+            Stream                  stream;
+            StreamReader            reader;
 
 
             Console.WriteLine("start");
+
+            #region UdgerParse
             // Create a new UdgerParser object
             UdgerParser parser = new UdgerParser();
 
             // Set data dir (in this directory is stored data file: udgerdb_v3.dat)
-            // Data file can be downloaded manually from http://data.udger.com/, but we recommend use udger-updater
+            // Data file can be downloaded from http://data.udger.com/
             parser.SetDataDir(@"C:\udger");
+            //parser.SetDataDir(@"C:\udger", "udgerdb_v3-noip.dat ");
+            #endregion
 
+            #region IP test
+            Console.WriteLine("download test IP file start");
+            client = new WebClient();
+            stream = client.OpenRead("https://raw.githubusercontent.com/udger/test-data/master/test_ua-ip/ip_1000.txt");
+            reader = new StreamReader(stream);
+            Console.WriteLine("download test IP file end");
+
+
+            Console.WriteLine("parse IP start");
+            start_time = DateTime.Now.Ticks;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                parser.ip = line;
+                // Parse
+                parser.parse();
+                i = parser.ipAddress;
+            }
+
+
+            elapsed_time = (DateTime.Now.Ticks - start_time) / 10;
+            Console.WriteLine("parse IP end, time: " + elapsed_time);                       
+            #endregion
+
+            #region UA test
             Console.WriteLine("download test UA file start");
-            WebClient client = new WebClient();
-            Stream stream = client.OpenRead("https://raw.githubusercontent.com/udger/test-data/master/test_ua-ip/ua_1000.txt");
-            StreamReader reader = new StreamReader(stream);            
+            client = new WebClient();
+            stream = client.OpenRead("https://raw.githubusercontent.com/udger/test-data/master/test_ua-ip/ua_1000.txt");
+            reader = new StreamReader(stream);            
             Console.WriteLine("download test UA file end");
 
 
@@ -54,10 +87,12 @@ namespace test_ua_ip
 
             elapsed_time = (DateTime.Now.Ticks - start_time) /10;
             Console.WriteLine("parse UA end, time: " + elapsed_time);
+            #endregion
 
+            Console.WriteLine("end");
             // Suspend the screen.
             Console.ReadLine();
-
+            
         }
     }
 }

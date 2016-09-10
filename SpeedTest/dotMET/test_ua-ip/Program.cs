57beb61e41c950394461f3d0abc3c7b85fc4ab69
udger.com <info@udger.com>
2016-09-10 12:36:01 +0200

    Initial commit

---
+++ SpeedTest/dotMET/test_ua-ip/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+ UdgerParser - test speed 
+ 
+  author     The Udger.com Team (info@udger.com)
+  license    GNU Lesser General Public License
+  link       http://udger.com/products/local_parser
+*/
+
+using Udger.Parser;
+using System.Net;
+using System.IO;
+using System;
+
+namespace test_ua_ip
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string line;
+            long start_time, elapsed_time;
+            Udger.Parser.UserAgent a;
+            Udger.Parser.IPAddress i;
+
+
+            Console.WriteLine("start");
+            // Create a new UdgerParser object
+            UdgerParser parser = new UdgerParser();
+
+            // Set data dir (in this directory is stored data file: udgerdb_v3.dat)
+            // Data file can be downloaded manually from http://data.udger.com/, but we recommend use udger-updater
+            parser.SetDataDir(@"C:\udger");
+
+            Console.WriteLine("download test UA file start");
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://raw.githubusercontent.com/udger/test-data/master/test_ua-ip/ua_1000.txt");
+            StreamReader reader = new StreamReader(stream);            
+            Console.WriteLine("download test UA file end");
+
+
+            Console.WriteLine("parse UA start");
+            start_time = DateTime.Now.Ticks;
+
+            while ((line = reader.ReadLine()) != null)
+            {                
+                line = line.Trim();
+                parser.ua = line;
+                // Parse
+                parser.parse();
+                a = parser.userAgent;
+            }
+
+
+            elapsed_time = (DateTime.Now.Ticks - start_time) /10;
+            Console.WriteLine("parse UA end, time: " + elapsed_time);
+
+            // Suspend the screen.
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SpeedTest/dotMET/test_ua-ip/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("test_ua-ip")]
+[assembly: AssemblyDescription("UdgerParser - test speed")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Udger s.r.o.")]
+[assembly: AssemblyProduct("test_ua-ip")]
+[assembly: AssemblyCopyright("Copyright © Udger s.r.o. 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("95ef26ca-9dd0-483c-b2d1-0f553a69bc19")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
